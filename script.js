c4163907adc8230a8bfdd45fea5415375a7f5c80
WalidElmiloudi <elwalid38@gmail.com>
2025-11-14 22:09:03 +0100

    adding some functionality to the play page

--- script.js
+++ script.js
@@ -21,18 +21,27 @@ let allCards = [];
 let deck = [];
 let Hand = [0, 0, 0, 0, 0];
 let inHand = [];
-const playDeck = JSON.parse(localStorage.getItem("deck")) || [];
+let handCount = 0;
+let opposantHandCount = 0;
+let opposantHand = [0, 0, 0, 0, 0];
+let opposantCards = [];
+
+let playDeck = JSON.parse(localStorage.getItem("deck")) || [];
+let opposantPlayDeck = JSON.parse(localStorage.getItem("Cards")) || [];
 
 async function loadCards() {
   const res = await fetch("cards.json");
   const data = await res.json();
   allCards = data.cards;
+  localStorage.setItem("Cards", JSON.stringify(allCards));
   displayCards(allCards);
 }
 
 function displayCards(cards) {
   const container = document.getElementById("cardsDisplay");
-  container.innerHTML = "";
+  if (container) {
+    container.innerHTML = "";
+  }
 
 
   cards.forEach(card => {
@@ -119,7 +128,9 @@ function displayCards(cards) {
     </div>
   </div>
     `;
-    container.appendChild(cardDiv);
+    if (container) {
+      container.appendChild(cardDiv);
+    }
     const favBtn = cardDiv.querySelector(`.fav-${card.id}`);
     favBtn.addEventListener("click", () => {
 
@@ -224,7 +235,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function displayFavorites() {
   const favContainer = document.getElementById("favoritesDisplay");
-  favContainer.innerHTML = "";
+  if (favContainer) {
+    favContainer.innerHTML = "";
+  }
 
   const favorites = JSON.parse(localStorage.getItem("favorites")) || [];
 
@@ -316,7 +329,9 @@ function displayFavorites() {
     </div>
   </div>
     `;
-    favContainer.appendChild(cardDiv);
+    if (favContainer) {
+      favContainer.appendChild(cardDiv);
+    }
     const favBtn = cardDiv.querySelector(`.fav-${card.id}`);
     favBtn.addEventListener("click", () => {
       let storedFavs = JSON.parse(localStorage.getItem("favorites")) || [];
@@ -352,13 +367,17 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 function displayCarts() {
   const cartContainer = document.getElementById("cartDisplay");
-  cartContainer.innerHTML = "";
+  if (cartContainer) {
+    cartContainer.innerHTML = "";
+  }
 
   const carts = JSON.parse(localStorage.getItem("carts")) || [];
 
   if (carts.length === 0) {
-    cartContainer.innerHTML = "<p class='text-black text-xl font-bold'>Nothing added to cart yet!</p>";
-    return;
+    if (cartContainer) {
+      cartContainer.innerHTML = "<p class='text-black text-xl font-bold'>Nothing added to cart yet!</p>";
+      return;
+    }
   }
 
   carts.forEach((card, index) => {
@@ -387,9 +406,6 @@ function displayCarts() {
           </div>
           <div class="w-2 h-2 border border-[#bea301] bg-black absolute rotate-45 -right-1 top-0.5"></div>
           <div class="w-1 h-1 border border-[#bea301] bg-black absolute rotate-45 -right-1.5 top-1"></div>
-        </div>
-        <div>
-
         </div>
         <div class="flex relative items-center -mt-2">
           <div class="flex items-center relative -top-0.25">
@@ -447,7 +463,9 @@ function displayCarts() {
                 class="fi fi-rr-cross-small text-base text-[#BEA301] pt-1.5"></i></div>
           </div>
     `;
-    cartContainer.appendChild(cardDiv);
+    if (cartContainer) {
+      cartContainer.appendChild(cardDiv);
+    }
     const rmBtn = cardDiv.querySelector(`.rm-${card['id']}`);
     rmBtn.addEventListener("click", () => {
       let storedCarts = JSON.parse(localStorage.getItem("carts")) || [];
@@ -548,7 +566,9 @@ cartIcon.forEach((cart) => {
 
 function deckDisplay() {
   const deckCardsContainer = document.getElementById("deckDisplay");
-  deckCardsContainer.innerHTML = "";
+  if (deckCardsContainer) {
+    deckCardsContainer.innerHTML = "";
+  }
 
   const deck = JSON.parse(localStorage.getItem("deck")) || [];
 
@@ -621,11 +641,12 @@ function deckDisplay() {
       <div class="flex items-center justify-center">
         <p class="text-sm text-white">Quantity : <span>${card.quantityOrder || 1}</span></p>
       </div>
-      
     </div>
   </div>
     `;
-    deckCardsContainer.appendChild(cardDiv);
+    if (deckCardsContainer) {
+      deckCardsContainer.appendChild(cardDiv);
+    }
   });
 }
 
@@ -677,29 +698,39 @@ window.addEventListener("scroll", function () {
 });
 
 const q1 = document.querySelector(".q1")
-q1.addEventListener("click", () => {
-  document.querySelector(".a1").classList.toggle("hidden");
-});
+if (q1) {
+  q1.addEventListener("click", () => {
+    document.querySelector(".a1").classList.toggle("hidden");
+  });
+}
 
-const q2 = document.querySelector(".q1")
-q2.addEventListener("click", () => {
-  document.querySelector(".a2").classList.toggle("hidden");
-});
+const q2 = document.querySelector(".q2")
+if (q2) {
+  q2.addEventListener("click", () => {
+    document.querySelector(".a2").classList.toggle("hidden");
+  });
+}
 
-const q3 = document.querySelector(".q1")
-q3.addEventListener("click", () => {
-  document.querySelector(".a3").classList.toggle("hidden");
-});
+const q3 = document.querySelector(".q3")
+if (q3) {
+  q3.addEventListener("click", () => {
+    document.querySelector(".a3").classList.toggle("hidden");
+  });
+}
 
-const q4 = document.querySelector(".q1")
-q4.addEventListener("click", () => {
-  document.querySelector(".a4").classList.toggle("hidden");
-});
+const q4 = document.querySelector(".q4")
+if (q4) {
+  q4.addEventListener("click", () => {
+    document.querySelector(".a4").classList.toggle("hidden");
+  });
+}
 
-const q5 = document.querySelector(".q1")
-q5.addEventListener("click", () => {
-  document.querySelector(".a5").classList.toggle("hidden");
-});
+const q5 = document.querySelector(".q5")
+if (q5) {
+  q5.addEventListener("click", () => {
+    document.querySelector(".a5").classList.toggle("hidden");
+  });
+}
 
 function playDisplay(deck) {
   const ownedCardsContainer = document.getElementById("playDeckDisplay");
@@ -777,25 +808,23 @@ function playDisplay(deck) {
 
 function updateDeckCounter(storeddeck) {
   const counterElement = document.querySelector(".deckCounter");
-  let counter = storeddeck.length
-  if (counterElement) {
-    counterElement.textContent = counter;
-    counter--;
-  } else {
-    console.error("Counter element with id 'counts' not found!");
-  }
+  let counter = 0;
+  storeddeck.forEach(card => {
+    counter += card.quantityOrder;
+  })
+  counterElement.textContent = counter;
+  counter--;
 }
 
-let inHandCount = 0;
 
 function drawCard() {
-  const index = playDeck.length;
-  const randCard = Math.floor(Math.random() * index);
-  const card = playDeck[randCard];
+  let StartGameDraw = 0;
+  const summonAvaibality = document.getElementById("summon");
   const drawing = document.querySelector("#Drawer");
-let count = 0;
+  let count = 0;
   drawing.addEventListener("click", () => {
     for (let i = 0; i < Hand.length; i++) {
+      const index = playDeck.length;
       const randCard = Math.floor(Math.random() * index);
       const card = playDeck[randCard];
       if (Hand[i] === 0) {
@@ -819,9 +848,6 @@ let count = 0;
           </div>
           <div class="w-2 h-2 border border-[#bea301] bg-black absolute rotate-45 -right-1 top-0.5"></div>
           <div class="w-1 h-1 border border-[#bea301] bg-black absolute rotate-45 -right-1.5 top-1"></div>
-        </div>
-        <div>
-
         </div>
         <div class="flex relative items-center -mt-2">
           <div class="flex items-center relative -top-0.25">
@@ -858,15 +884,33 @@ let count = 0;
     </div>
     `;
         Hand[i] = 1;
-        // document.getElementById("inhand").textContent = 
-        playDeck.splice(randCard, 1);
-        updateDeckCounter(playDeck);
-        playDisplay(playDeck);
-        count++;
-        break;
+        summonAvaibality.textContent = "Used"
+        drawing.classList.replace("bg-[#bea301]", "bg-[#ece5bf]");
+        drawing.classList.replace("cursor-pointer", "cursor-not-allowed")
+        drawing.setAttribute("disabled", "true");
+        handCount++;
+        if (handCount <= 5) {
+          document.getElementById("inhand").textContent = handCount;
+        } else {
+          document.getElementById("inhand").textContent = "0";
+        }
+        if (playDeck[randCard].quantityOrder > 1) {
+          playDeck[randCard].quantityOrder--;
+          updateDeckCounter(playDeck);
+          count++;
+        } else {
+          playDeck.splice(randCard, 1);
+          updateDeckCounter(playDeck);
+          playDisplay(playDeck);
+          count++;
+        }
+        if (StartGameDraw === 1) {
+          break;
+        }
       }
 
     }
+    StartGameDraw = 1;
   })
 
 }
@@ -884,17 +928,24 @@ function dropHandler(ev) {
   const atkBtn = document.getElementById("atk");
   const defBtn = document.getElementById("def");
 
+
   if (ev.target.classList.contains('slot') && ev.target.childElementCount == 0) {
     ev.target.appendChild(document.getElementById(data));
-    if (!(ev.target.classList.contains('atk')) && !(ev.target.classList.contains('def'))) {
-      ev.target.addEventListener("click", () => {
+    const slotsAvailability = document.querySelectorAll(".slot");
+    slotsAvailability.forEach(slot => {
+      slot.removeAttribute("ondrop", "ondragover");
+    })
+    let match = data.match(/card-([^\s]+)-[^\s]+/);
+    if (match) {
+      let index = parseInt(match[1]);
+      Hand[index - 1] = 0;
+      handCount--;
+      document.getElementById("inhand").textContent = handCount;
+    }
+    ev.target.addEventListener("click", () => {
+      if (!(ev.target.classList.contains('atk')) && !(ev.target.classList.contains('def'))) {
         const statutTab = document.getElementById("statut");
         statutTab.classList.toggle("hidden");
-
-        const currentTarget = ev.target;
-
-        const atkBtn = document.getElementById("atk");
-        const defBtn = document.getElementById("def");
 
         atkBtn.replaceWith(atkBtn.cloneNode(true));
         defBtn.replaceWith(defBtn.cloneNode(true));
@@ -903,22 +954,179 @@ function dropHandler(ev) {
         const defBtnNew = document.getElementById("def");
 
         atkBtnNew.addEventListener("click", () => {
-          currentTarget.classList.remove('def', 'rotate-90', 'scale-70', 'bg-green-600');
-          currentTarget.classList.add('bg-red-600', 'atk');
+          ev.target.classList.remove('def', 'rotate-90', 'scale-70', 'bg-green-600');
+          ev.target.classList.add('bg-red-600', 'atk');
           statutTab.classList.toggle("hidden");
         })
 
         defBtnNew.addEventListener("click", () => {
-          currentTarget.classList.remove('atk', 'bg-red-600');
-          currentTarget.classList.add('rotate-90', 'scale-70', 'bg-green-600', 'def');
+          ev.target.classList.remove('atk', 'bg-red-600');
+          ev.target.classList.add('rotate-90', 'scale-70', 'bg-green-600', 'def');
           statutTab.classList.toggle("hidden");
         })
-      })
+      }
+    })
+  }
+}
+const endTurnBtn = document.getElementById("endTurn");
+let turn = 0;
+endTurnBtn.addEventListener("click", () => {
+  const hands = document.querySelectorAll(".hand");
+  const slots = document.querySelectorAll(".slot");
+  let isHandempty = 0;
+  let isSlotempty = 0;
+  let i = 0;
+
+  for (i = 0; i < hands.length; i++) {
+    if (hands[i].childElementCount != 0) {
+      isHandempty = 1;
+      break;
     }
   }
-  let match = data.match(/card-([^\s]+)-[^\s]+/);
-  if (match) {
-    let index = parseInt(match[1]);
-    Hand[index - 1] = 0;
+  for (i = 0; i < slots.length; i++) {
+    if (slots[i].childElementCount != 0 && (slots[i].classList.contains("atk") || slots[i].classList.contains("def"))) {
+      isSlotempty = 1;
+      break;
+    }
   }
+  if (isHandempty == 0 || isSlotempty == 0) {
+    alert("You Need To Play And Draw A Card!!");
+
+  } else {
+    endTurnBtn.classList.remove("bg-blue-600", "cursor-pointer");
+    endTurnBtn.classList.add("bg-gray-600", "cursor-not-allowed");
+    endTurnBtn.setAttribute("disabled", "true");
+    document.getElementById("turn").textContent = "Opposant Turn";
+    turn = 1;
+    opposant();
+  }
+})
+let StartOppGameDraw = 0;
+function opposant() {
+  console.log(opposantHandCount);
+  const summonAvaibality = document.getElementById("summon");
+  const drawing = document.querySelector("#Drawer");
+
+  if (turn == 1) {
+    for (let i = 0; i < opposantHand.length; i++) {
+      const index = opposantPlayDeck.length;
+      const randCard = Math.floor(Math.random() * index);
+      const card = opposantPlayDeck[randCard];
+      const storedQuantities = JSON.parse(localStorage.getItem("quantities")) || quantities;
+      let quantity = storedQuantities[card.rarity];
+      if (opposantHand[i] === 0) {
+        opposantCards[i] = opposantPlayDeck[randCard];
+      }
+      opposantHand[i] = 1;
+      opposantHandCount++;
+      if (opposantHandCount <= 5) {
+        document.getElementById("opphand").textContent = opposantHandCount;
+        console.log(opposantHandCount);
+      } else {
+        document.getElementById("opphand").textContent = "0";
+      }
+      if (quantity > 1) {
+        quantity--;
+      } else {
+        opposantPlayDeck.splice(randCard, 1);
+      }
+      if (StartOppGameDraw === 1) {
+        break;
+      }
+    }
+  }
+  StartOppGameDraw = 1;
+
+  const opposantSlots = document.querySelectorAll(".oppSlot");
+  console.log(opposantHandCount);
+  for (let i = 0; i < opposantSlots.length; i++) {
+    if (opposantSlots[i].childElementCount == 0) {
+      const cardOpp = opposantCards[i];
+      const cardDiv = document.createElement('div');
+      cardDiv.innerHTML = `
+             <div class="w-35.5 h-47.5 bg-black flex justify-center items-center scale-38 xl:scale-80 2xl:scale-115 cursor-pointer" draggable="true"
+                ondragstart="dragstartHandler(event)">
+          <div class="w-33 h-45.25 border border-[#bea301] flex flex-col items-center justify-evenly overflow-hidden">
+            <div class="w-31  border border-[#bea301] relative">
+              <img draggable = 'false'   class="animate-pulse" src="${cardOpp['bg-img']}" alt="${cardOpp['rarity']} background">
+              <img draggable = 'false'  class="absolute bottom-0 right-0" src="${cardOpp['caracter']}" alt="${cardOpp['name']}">
+              <div
+                class="w-3.5 h-3.5 bg-black border border-[#bea301] absolute z-20 flex justify-center items-center top-0 rotate-45">
+                <p class="${cardOpp['txt-color']} text-xs -rotate-45">${cardOpp['power']}</p>
+              </div>
+            </div>
+            <div class="flex relative -mt-0.5">
+              <div class="w-1 h-1 border border-[#bea301] bg-black absolute rotate-45 -left-1.5 top-1 z-10"></div>
+              <div class="w-2 h-2 border border-[#bea301] bg-black absolute rotate-45 -left-1 top-0.5"></div>
+              <div class="w-21.5 h-3 border border-[#bea301] flex justify-center items-center rounded-full">
+                <p class="${cardOpp['txt-color']} text-[6px]">${cardOpp['name']}</p>
+              </div>
+              <div class="w-2 h-2 border border-[#bea301] bg-black absolute rotate-45 -right-1 top-0.5"></div>
+              <div class="w-1 h-1 border border-[#bea301] bg-black absolute rotate-45 -right-1.5 top-1"></div>
+            </div>
+            <div>
+
+            </div>
+            <div class="flex relative items-center -mt-2">
+              <div class="flex items-center relative -top-0.25">
+                <div class="w-0.75 h-0.75 bg-[#BEA301] rotate-45"></div>
+                <hr class="w-5 border-0.5 border-[#BEA301]">
+              </div>
+              <p class="text-[7px] ${cardOpp['txt-color']}">${cardOpp['rarity']}</p>
+              <div class="flex items-center relative -top-0.25">
+                <hr class="w-5 border-0.5 border-[#BEA301]">
+                <div class="w-0.75 h-0.75  bg-[#BEA301] rotate-45"></div>
+              </div>
+            </div>
+            <p class="text-[5px] ${cardOpp['txt-color']} -mt-1.75">${cardOpp['role']}</p>
+            <div class="border border-[#BEA301] py-0.25 px-1 flex items-center justify-center rounded-full -mt-1">
+              <p class="text-[5px] ${cardOpp['txt-color']}">${cardOpp['element']}</p>
+            </div>
+            <div class="flex relative w-full justify-center items-end">
+              <div class="flex flex-col items-center absolute -bottom-1 left-0.5">
+                <p class="text-[5px] text-[#BEA301]">${cardOpp['atk']} Pt</p>
+                <div class="border border-[#BEA301] flex items-center justify-center rounded-full w-5.5 h-5.5">
+                  <img draggable = 'false'  src="imgs/atk icon.png" alt=" swords">
+                </div>
+              </div>
+              <p class="text-[4.5px] text-[#bea301]">"<span class="${cardOpp['txt-color']}"> ${cardOpp['description']} </span>"</p>
+              <div class="flex flex-col items-center absolute -bottom-1 right-0.5">
+                <p class="text-[5px] text-[#BEA301]">${cardOpp['def']} Pt</p>
+                <div class="border border-[#BEA301] flex items-center justify-center rounded-full w-5.5 h-5.5">
+                  <img draggable = 'false'  src="imgs/def icon.png" alt=" shield">
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        </div>
+        `;
+      opposantSlots[i].appendChild(cardDiv);
+      opposantHand[i] = 0;
+      opposantHandCount--;
+      document.getElementById("opphand").textContent = opposantHandCount;
+      const randStatut = Math.floor(Math.random() * 2);
+      if (randStatut == 1) {
+        opposantSlots[i].classList.add('bg-red-600', 'atk');
+      } else {
+        opposantSlots[i].classList.add('rotate-90', 'scale-70', 'bg-green-600', 'def');
+      }
+      endTurnBtn.classList.remove("bg-gray-600", "cursor-not-allowed");
+      endTurnBtn.classList.add("bg-blue-600", "cursor-pointer");
+      endTurnBtn.removeAttribute("disabled");
+      document.getElementById("turn").textContent = "Your Turn";
+      turn = 0;
+      summonAvaibality.textContent = "Available"
+      drawing.classList.replace("bg-[#ece5bf]", "bg-[#bea301]");
+      drawing.classList.replace("cursor-not-allowed", "cursor-pointer")
+      drawing.removeAttribute("disabled");
+      const slotsAvailability = document.querySelectorAll(".slot");
+      slotsAvailability.forEach(slot => {
+        slot.setAttribute("ondrop", "dropHandler(event)");
+        slot.setAttribute("ondragover", "dragoverHandler(event)");
+      })
+      break;
+    }
+  }
+
 }
